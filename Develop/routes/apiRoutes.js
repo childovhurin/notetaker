555f9f96079f50165149c00c6f89bacab4883c3b
Megan Murphy <childovhurin@gmail.com>
2020-04-25 16:40:50 -0400

    added package lock json, started get function for api

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -5,3 +5,23 @@ const db = require("../db/db.json");
 
 //unique user id
 const uuid = require("uuid/v4");
+
+//router
+router.get("/api/notes", function (req, res) {
+    res.send(db);
+});
+router.post("/api/notes", function (req, res) {
+    // uuid called to generate random id
+    const noteID = uuid();
+    // object array for new note
+    let newNote = {
+        id: noteId,
+        title: req.body.title,
+        text: req.body.text,
+    };
+
+});
+module.exports = router;
+
+
+ 
